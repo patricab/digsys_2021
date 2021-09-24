e3c3e1e923b821e31a1cb1efde066561a68316d7
ejaanens <ejaanens@stud.ntnu.no>
2021-09-24 19:13:46 +0200

    gen primes, modulo exp function decryption key gen still not working

--- src/rsa.py
+++ src/rsa.py
@@ -1,10 +1,37 @@
 #!/usr/bin/env python3
+from secrets import randbits
 
-# from math import ceil
+
+def isPrime(n):
+    if (n % 2 == 0 or n % 3 == 0):
+        return False
+    i = 5
+    while(i * i <= n):
+        if (n % i == 0 or n % (i + 2) == 0):
+            return False
+        i = i + 6
+        return True
+
+
+def genprime(b):
+    p = 4
+    notprime = True
+    while notprime:
+        p = 2**b + 2**(b - 1) + 2 * randbits(b - 2) + 1
+        notprime = isPrime(p)
+    return p
+
+
+def genprimes(k):
+    k /= 2
+    k -= 1
+    b = int(k)
+    p = genprime(b)
+    q = genprime(b)
+    return p, q
+
 
 # greatest common divisor
-
-
 def gcd(a, b):
     if (a == 0):
         return b
@@ -20,50 +47,58 @@ def phi(n):
     return result
 
 
-def ModExpOp(m, key, n):
-    c = 0
-    key = format(key, 'b')
-    print(key)
-    if (key[-1] == 1):
-        c = m
-    else:
-        c = 1
-    print(c)
+def modmult(a, b, n):
+    p = 0
+    b = format(b, 'b')
+    for i in range(0, len(b)):
+        p = 2 * p + a * int(b[i])
+        if p >= n:
+            p -= n
+        if p >= n:
+            p -= n
+    return p
 
-    for i in range(len(key) - 2, -1, -1):
-        c = c * c % n
-        print(c)
-        if (key[i] == 1):
-            c = c * m % n
-            print(c)
+
+def modexp(m, key, n):
+    key = format(key, 'b')
+    c = 1
+    p = m
+    for i in range(0, len(key)):
+        if (key[-1 - i] == '1'):
+            c = modmult(c, p, n)
+        p = modmult(p, p, n)
     return c
 
 
 # n, e, d 256bit
+b = 256
+
+k = 1  # any integer
+
 e = 0
 d = 0.1
 
+# check if prime (randomly generate)
+(p, q) = genprimes(b)
+
+# p = 6795469  # large prime
+# q = 6799291  # large prime
+
+n = p * q
+
+print(str(len(format(n, 'b'))) + 'bit encryption')
 
 message = "a"
 print('input:  ' + message)
+m = int.from_bytes(message.encode('utf-8'), 'little')
+print('message# in: ' + str(m))
 
-p = 673982395699  # large prime
-q = 773982392009  # large prime
-# check if prime (randomly generate)
-# sqrt(p)
-k = 1   # any integer
-
-M = int.from_bytes(message.encode('utf-8'), 'little')
-print('message# in: ' + str(M))
-n = p * q
-print(len(format(n, 'b')))
-
-if (M > n):
+if (m > n):
     print('message to large')
 
 Φ = (p - 1) * (q - 1)  # easy way to find phi(n)
 
-for i in range(int(Φ / 2**32), int(Φ / 2)):
+for i in range(2**15 - 1, int(Φ / 2)):
     e = 2 * i + 1
     if (gcd(e, Φ) == 1):
         break
@@ -82,14 +117,14 @@ if ((d * e) % Φ != 1) | (d > n):
     # print(n - d)
     print('creating decryption key failed')
 else:
-    # C = M**e % n
-    C = pow(M, e, n)
+    # c = m**e % n
+    c = modexp(m, e, n)
 
-    # M = C**d % n
-    M = pow(C, d, n)
+    # m = c**d % n
+    m = modexp(c, d, n)
 
-    print('message# out: ' + str(M))
+    print('message# out: ' + str(m))
 
-    decrypted = M.to_bytes((M.bit_length() + 7) // 8, 'little').decode('utf-8')
+    decrypted = m.to_bytes((m.bit_length() + 7) // 8, 'little').decode('utf-8')
 
     print('output: ' + decrypted)
